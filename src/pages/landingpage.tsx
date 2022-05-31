e6c221a77de0d147a68941548a0d2e4ecc243470
emilierave <marjabronikowska@gmail.com>
2022-05-31 11:54:41 +0200

    feat(home) landingpage app change

---
+++ src/pages/landingpage.tsx
@@ -0,0 +1,43 @@
+// @ts-ignore
+import logo from "../DSC00202-copy.svg";
+import React from 'react';
+import InputForm from "./InputForm";
+import './pagesStyles.scss'
+
+
+export const Home= () =>{
+    const dialogAlert = () => {
+        alert('There is no hint in here')
+    }
+    const dialogAlert2 = () => {
+        alert('There is no good or bad name, it is just a name, do not worry')
+    }
+    return (
+        <main>
+            <div className="landing">
+                <header className="landing-header">
+                    <img src={logo} className="landing-logo" alt="logo"  />
+                    <p>
+                        Give a name to an abstract woman above
+                    </p>
+                    <button onClick={dialogAlert}>Give Me a hint</button>
+                    <button onClick={dialogAlert2}>Any hint, please</button>
+                    <InputForm/>
+                    <a
+                        className="landing-link"
+                        href="https://github.com/emilierave/daftcode-react-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >link to git repo</a>
+                    <a
+                        className="landing-link"
+                        href="https://en.wikipedia.org/wiki/Edge_cover"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >edge cover</a>
+                </header>
+            </div>
+        </main>
+    );
+};
+export default Home;
