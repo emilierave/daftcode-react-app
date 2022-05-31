a36342ac70d69b76250cab30d24362d02917ee81
emilierave <marjabronikowska@gmail.com>
2022-05-31 11:10:32 +0200

    feat(app) add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,30 @@
-// @ts-ignore
-import logo from "./DSC00202-copy.svg";
-import React from 'react';
-import InputForm from "./home";
-import './App.css';
+
+
+import './App.scss';
+
+import InputForm from './pages/InputForm'
+
+import {Navbar} from './pages/Nav/Navbar'
+import {Home} from './pages/landingpage'
+import { Routes, Route } from 'react-router-dom';
+import Contact from './pages/contacpage'
+import About from './pages/aboutpage'
+
 function App() {
-  const dialogAlert = () => {
-    alert('There is no hint in here')
-  }
-  const dialogAlert2 = () => {
-    alert('There is no good or bad name, it is just a name, do not worry')
-  }
+
   return (
-      <main>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Give a name to an abstract woman above
-        </p>
-        <button onClick={dialogAlert}>Give Me a hint</button>
-        <button onClick={dialogAlert2}>Any hint, please</button>
-        <InputForm/>
-        <a
-            className="App-link"
-            href="https://github.com/emilierave/daftcode-react-app"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          link to git repo
-        </a>
-        <a
-            className="App-link"
-            href="https://en.wikipedia.org/wiki/Edge_cover"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          edge cover
-        </a>
-      </header>
-    </div>
-  </main>
+      <div className="App">
+<Navbar/>
+        <div className="header">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/form' element = {<InputForm  defaultValue={'Hello There'}/>} />
+          </Routes>
+        </div>
+
+      </div>
   );
 }
 
