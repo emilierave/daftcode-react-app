e3e792cdaa1b3a972903c70e2698b5965b1101f4
emilierave <marjabronikowska@gmail.com>
2022-06-07 11:47:22 +0200

    feat(app) add routing

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,8 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
             <Route path='/contact' element={<Contact />} />
-            <Route path='/form' element = {<InputForm  defaultValue={'Hello There'}/>} />
+            <Route path='/form' element = {<InputForm/>} />
+              <Route path ='/rick' element={ <Rick/>} />
           </Routes>
         </div>
 
